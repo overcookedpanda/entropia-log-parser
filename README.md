740dc0c9d5551d91461be08173e23bcf58a6178e
Overcooked Panda <overcookedpanda@gmail.com>
2019-12-27 19:27:34 -0600

    changed output to stdout added configurable options for cost calculations

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 Simple script to parse Entropia Universe chat.log for various statistics using regex.
 
+Configurable options:
+
+# Define Player Variables
+player = "Overcooked OC Panda"
+# MarCorp Kallous 7 + Omegaton A105 Improved + Weapon Damage Enhancers 1-9
+weaponCost = 0.24
+weaponDecay = 0.0466
+# Mark Up in percentage points, ie) 115% = 1.15
+weaponMarkUp = 1
+# Weapon Damage Enhancers 1-9
+enhancerCost = 0.8
+# Average Mark Up in percentage points, ie) 310% = 3.1
+enhancerMarkUp = 3.1
+# Polaris (L) + Armor Plating Mk. 5B dmg/pec
+armorCost = 22.27
+# Average Mark Up in percentage points, ie) 110% = 1.1
+armorMarkUp = 1.1
+# Adjusted Restoration Chip heal/pec
+healCost = 159.22
+healDecay = 0
+# Other Costs, pills, etc, user specified in PED, for items used during hunt.
+otherCosts = 0
+
+
 Currently you run it with ./entropia-log-parser.py chat.log and it will output .csv files into ./parsed_logs which can then be further processed.
+
+Example Output:
+
+Processed 51972 lines...
+Total Damage Taken: 14106.60 Armor Hit Points
+Total Damage Cost: 7.04 PED
+---
+Total Heals: 11081.00 Health Points
+Total Heal Cost: 0.70 PED
+---
+Total Broken Enhancers: 65
+Total Broken Enhancer NET Cost: 109.20 PED
+---
+Total Shots taken: 29815
+Total Damage Inflicted: 2551890.30 Mob Hit Points
+Total Weapon Cost: 8654.18 PED
+Other User Specified Costs: 0 PED
+---
+Total Cycled: 8661.91 PED
+Average Eco: 2.95 DPP
+---
+UL Tier Increases During Run:
+2019-12-26 16:55:11 :: MarCorp Kallous-7 - 9.63
+2019-12-26 17:16:03 :: MarCorp Kallous-7 - 9.64
+2019-12-27 12:08:28 :: MarCorp Kallous-7 - 9.65
+---
+Total Global Loot: 2007.0 PED
+Total Skills Gained: 1146.84
+
--- entropia-log-parser.py
+++ entropia-log-parser.py
@@ -13,6 +13,15 @@ def main():
 
     with open(filepath) as fp:
         counter = 0
+        shots = 0
+        hits = 0
+        totalDmgTaken = 0
+        totalDmgInflicted = 0
+        totalSkills = 0
+        totalHeals = 0
+        totalGlobals = 0
+        totalBrokenEnhancers = 0
+        totalTierIncreases = []
         for line in fp:
             attributeGains = get_attribute_gains(line)
             skillGains = get_skill_gains(line)
@@ -21,39 +30,80 @@ def main():
             dmgInflicted = get_dmg_inflicted(line)
             dmgTaken = get_dmg_taken(line)
             playerGlobals = get_player_globals(player, line)
+            brokenEnhancers = get_broken_enhancers(line)
             if skillGains != (None, None, None):
-                f = open("./parsed_logs/skill_gains.csv", "a+")
-                f.write(skillGains[0] + "," + skillGains[1] + "," + skillGains[2] + '\n')
-                f.close()
+                #f = open("./parsed_logs/skill_gains.csv", "a+")
+                #f.write(skillGains[0] + "," + skillGains[1] + "," + skillGains[2] + '\n')
+                #f.close()
+                totalSkills += float(skillGains[2])
             if attributeGains != (None, None, None):
-                f = open("./parsed_logs/skill_gains.csv", "a+")
-                f.write(attributeGains[0] + ',' + attributeGains[1] + ',' + attributeGains[2] + '\n')
-                f.close()
+                #f = open("./parsed_logs/skill_gains.csv", "a+")
+                #f.write(attributeGains[0] + ',' + attributeGains[1] + ',' + attributeGains[2] + '\n')
+                #f.close()
+                totalSkills += float(attributeGains[2])
             if tierIncreases != (None, None, None):
                 #  Do not track tier increases on (L) items.
                 if not re.search(r'\(M,L\)|\(L\)', tierIncreases[1]):
-                    f = open("./parsed_logs/tier_increases.csv", "a+")
-                    f.write(tierIncreases[0] + "," + tierIncreases[1] + ',' + tierIncreases[2] + "\n")
-                    f.close()
+                    #f = open("./parsed_logs/tier_increases.csv", "a+")
+                    #f.write(tierIncreases[0] + "," + tierIncreases[1] + ',' + tierIncreases[2] + "\n")
+                    #f.close()
+                    totalTierIncreases.append(tierIncreases)
             if selfHeals != (None, None, None):
-                f = open("./parsed_logs/self_heals.csv", "a+")
-                f.write(selfHeals[0] + "," + selfHeals[1] + ',' + selfHeals[2] + "\n")
-                f.close()
+                #f = open("./parsed_logs/self_heals.csv", "a+")
+                #f.write(selfHeals[0] + "," + selfHeals[1] + ',' + selfHeals[2] + "\n")
+                #f.close()
+                totalHeals += float(selfHeals[2])
             if dmgInflicted != (None, None, None):
-                f = open("./parsed_logs/damage_inflicted.csv", "a+")
-                f.write(dmgInflicted[0] + ',' + dmgInflicted[1] + ',' + dmgInflicted[2] + "\n")
-                f.close()
+                shots += 1
+                if not re.search(r'The target Dodged your attack', dmgInflicted[1]):
+                    #f = open("./parsed_logs/damage_inflicted.csv", "a+")
+                    #f.write(dmgInflicted[0] + ',' + dmgInflicted[1] + ',' + dmgInflicted[2] + "\n")
+                    #f.close()
+                    if dmgInflicted[2] != "":
+                        totalDmgInflicted += float(dmgInflicted[2])
             if dmgTaken != (None, None, None):
-                f = open("./parsed_logs/damage_taken.csv", "a+")
-                f.write(dmgTaken[0] + ',' + dmgTaken[1] + ',' + dmgTaken[2] + "\n")
-                f.close()
+                #f = open("./parsed_logs/damage_taken.csv", "a+")
+                #f.write(dmgTaken[0] + ',' + dmgTaken[1] + ',' + dmgTaken[2] + "\n")
+                #f.close()
+                hits += hits
+                totalDmgTaken += float(dmgTaken[2])
             if playerGlobals != (None, None, None):
-                f = open("./parsed_logs/globals.csv", "a+")
-                f.write(playerGlobals[0] + ',' + playerGlobals[1] + ',' + playerGlobals[2] + "\n")
-                f.close()
+                #f = open("./parsed_logs/globals.csv", "a+")
+                #f.write(playerGlobals[0] + ',' + playerGlobals[1] + ',' + playerGlobals[2] + "\n")
+                #f.close()
+                totalGlobals += float(playerGlobals[2])
+            if brokenEnhancers != (None, None, None, 0):
+                totalBrokenEnhancers += brokenEnhancers[3]
             counter += 1
-        print("Processed " + str(counter) + " lines...")
-
+    print("Processed " + str(counter) + " lines...")
+    print("Total Damage Taken: " + format(totalDmgTaken, '.2f') + " Armor Hit Points")
+    totalDmgCost = float((totalDmgTaken / (armorCost * (armorMarkUp - 2) * -1) * 0.01))
+    print("Total Damage Cost: " + format(totalDmgCost, '.2f') + " PED")
+    print("---")
+    print("Total Heals: " + format(totalHeals, '.2f') + " Health Points")
+    print("Total Heal Cost: " + format((totalHeals / healCost) * 0.01, '.2f') + " PED")
+    totalHealCost = float((totalHeals / healCost) * 0.01)
+    print("---")
+    print("Total Broken Enhancers: " + str(totalBrokenEnhancers))
+    totalEnhancerCost = float(totalBrokenEnhancers * ((enhancerCost * enhancerMarkUp) - enhancerCost))
+    print("Total Broken Enhancer NET Cost: " + format(totalEnhancerCost, '.2f') + " PED")
+    totalWeaponCost = float((shots * weaponCost) + (shots * (weaponDecay * weaponMarkUp)) + totalEnhancerCost)
+    print("---")
+    print("Total Shots taken: " + str(shots))
+    print("Total Damage Inflicted: " + format(totalDmgInflicted, '.2f') + " Mob Hit Points")
+    print("Total Weapon Cost: " + format(totalWeaponCost, '.2f') + " PED")
+    print("Other User Specified Costs: " + str(otherCosts) + " PED")
+    totalPedCycled = float(totalDmgCost + totalHealCost + totalWeaponCost + otherCosts)
+    print("---")
+    print("Total Cycled: " + format(totalPedCycled, '.2f') + " PED")
+    print("Average Eco: " + format(totalDmgInflicted / (totalPedCycled * 100), '.2f') + " DPP")
+    print("---")
+    print("UL Tier Increases During Run:")
+    for tier in totalTierIncreases:
+        print(tier[0] + " :: " + tier[1] + " - " + tier[2])
+    print("---")
+    print("Total Global Loot: " + str(totalGlobals) + " PED")
+    print("Total Skills Gained: " + format(totalSkills, '.2f'))
 
 # Attributes:
 def get_attribute_gains(data):
@@ -61,7 +111,7 @@ def get_attribute_gains(data):
     attribute = None
     points = None
     attribute_gains = re.findall(r'([0-9]{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:2[0-3]|[01][0-9]):['
-                                 r'0-5][0-9]:[0-6][0-9])(?: \[System\] \[\] )(?:.*gained )(\d.*.\d{0,4})\s(\w+)$',
+                                 r'0-5][0-9]:[0-6][0-9])(?: \[System\] \[\] )(?:.*gained )(\d.*.\d\d\d\d)\s(\w+)$',
                                  data)
 
     for attribute_gain in attribute_gains:
@@ -79,7 +129,7 @@ def get_skill_gains(data):
     points = None
     skill_gains = re.findall(
         r'([0-9]{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-6][0-9])(?: \['
-        r'System\] \[\] )(?:.*gained )(\d.*.\d{0,4})(?: .*your )(.*)(?: skill)',
+        r'System\] \[\] )(?:.*gained )(\d.*.\d\d\d\d)(?: .*your )(.*)(?: skill)',
         data)
 
     for skill_gain in skill_gains:
@@ -131,13 +181,15 @@ def get_dmg_inflicted(data):
     points = None
     text = None
     dmg_inflicted = re.findall(r'([0-9]{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:2[0-3]|[01][0-9]):[0-5]['
+                               r'0-9]:[0-6][0-9])(?: \[System\] \[\] )(?:.*?)(The target Dodged your attack)|([0-9]{'
+                               r'4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:2[0-3]|[01][0-9]):[0-5][ '
                                r'0-9]:[0-6][0-9])(?: \[System\] \[\] )(?:.*?)(?:You inflicted )(\d+.\d+)(?: points of '
                                r'damage)', data)
 
     for dmg in dmg_inflicted:
         date = dmg[0]
         text = 'Damage Inflicted'
-        points = dmg[1]
+        points = dmg[3]
 
     return date, text, points
 
@@ -165,8 +217,7 @@ def get_player_globals(player, data):
     amount = None
     player_globals = re.findall(r'([0-9]{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:2[0-3]|[01][0-9]):['
                                 r'0-5][0-9]:[0-6][0-9])(?: \[Globals\] \[\] )(?:' + player + ' )(?:killed a creature '
-                                                                                             r')\((.*?)\)(?:.* with a value of )(\d+)(?: PED!)',
-                                data)
+                                r')\((.*?)\)(?:.* with a value of )(\d+)(?: PED!)', data)
     for my_global in player_globals:
         date = my_global[0]
         creature = my_global[1]
@@ -175,8 +226,43 @@ def get_player_globals(player, data):
     return date, creature, amount
 
 
-# Define Player Name
+def get_broken_enhancers(data):
+    date = None
+    enhancer_type = None
+    weapon = None
+    broken = 0
+    broken_enhancers = re.findall(r'([0-9]{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1]) (?:2[0-3]|[01][0-9]):['
+                                  r'0-5][0-9]:[0-6][0-9])(?: \[System\] \[\] )(?:.*?)(?:Your enhancer )('
+                                  r'\S+\s\S+\s\S+\s\d)(?: on your )(.*)(broke)', data)
+    for enhancer in broken_enhancers:
+        date = enhancer[0]
+        enhancer_type = enhancer[1]
+        weapon = enhancer[2]
+        if enhancer[3] == "broke":
+            broken = 1
+    return date, enhancer_type, weapon, broken
+
+
+# Define Player Variables
 player = "Overcooked OC Panda"
+# MarCorp Kallous 7 + Omegaton A105 Improved
+weaponCost = 0.24
+weaponDecay = 0.0466
+# Mark Up in percentage points, ie) 115% = 1.15
+weaponMarkUp = 1
+# Weapon Damage Enhancers 1-9
+enhancerCost = 0.8
+# Average Mark Up in percentage points, ie) 310% = 3.1
+enhancerMarkUp = 3.1
+# Polaris (L) + Armor Plating Mk. 5B dmg/pec
+armorCost = 22.27
+# Average Mark Up in percentage points, ie) 110% = 1.1
+armorMarkUp = 1.1
+# Adjusted Restoration Chip heal/pec
+healCost = 159.22
+healDecay = 0
+# Other Costs, pills, etc, user specified in PED, for items used during hunt.
+otherCosts = 0
 
 # Attribute Gain function parse
 # log = '2019-12-23 02:04:59 [System] [] You have gained 0.0134 experience in your Anatomy skill' \
